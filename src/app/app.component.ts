c183fad822b0be0a77fbecf06f6f9eacf9eb7020
Julian Castellanos <jcastellanos003@gmail.com>
2018-12-17 13:02:12 -0500

    chore: test api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        const customHeaders = new HttpHeaders().append(
+            'Authorization',
+            'Bearer GfR6vIHG0zTWaJle6TjNXvYUrjDn6g'
+        );
+        /* headers = headers.append('Content-Type', 'application/json'); */
+        /* headers = headers.append('Access-Control-Allow-Origin', '*');
+        headers = headers.append('Access-Control-Allow-Credentials', 'true'); */
+        this.http
+            .get<any>('https://dev-api.toladata.io/api/workflowlevel1/', {
+                headers: customHeaders
+            })
+            .subscribe(
+                (data: any) => {
+                    console.log('data...', data);
+                },
+                err => console.log('errr=====', err)
+            );
+    }
+}
