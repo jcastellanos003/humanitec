c1e3001fa65903b318f13fe48458ff7620cec9a8
Julian Castellanos <jcastellanos003@gmail.com>
2018-12-19 12:02:56 -0500

    test(dashboard): fix tests
    fix tests

--- src/app/containers/app.component.spec.ts
+++ src/app/containers/app.component.spec.ts
@@ -1,27 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { LoadingComponent } from '../components';
+
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [AppComponent]
+            imports: [RouterTestingModule],
+            declarations: [AppComponent, LoadingComponent]
         }).compileComponents();
     }));
-    xit('should create the app', async(() => {
+    it('should create the app', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
-    }));
-    xit(`should have as title 'humanitec'`, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('humanitec');
-    }));
-    xit('should render title in a h1 tag', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain(
-            'Welcome to humanitec!'
-        );
     }));
 });
