1bf27484de622396b8ee0372cf310a475ed7c7c6
Julian Castellanos <jcastellanos003@gmail.com>
2018-12-20 23:59:51 -0500

    refactor(activities): Cleaned up code
    Cleaned up code

--- src/activities/components/activity-form/activity-form.component.ts
+++ src/activities/components/activity-form/activity-form.component.ts
@@ -8,21 +8,12 @@ import {
 } from '@angular/material/core';
 
 import { Activity } from '@humanitec/core';
-import { DEFAULT_DATE_FORMAT } from '@humanitec/utils';
+import {
+    DEFAULT_DATE_FORMAT,
+    DEFAULT_MATERIAL_DATE_FORMAT
+} from '@humanitec/utils';
 
 import * as moment from 'moment';
-
-export const MY_FORMATS = {
-    parse: {
-        dateInput: 'LL'
-    },
-    display: {
-        dateInput: 'DD/MM/YYYY',
-        monthYearLabel: 'MMM YYYY',
-        dateA11yLabel: 'LL',
-        monthYearA11yLabel: 'MMMM YYYY'
-    }
-};
 
 @Component({
     selector: 'activity-form',
@@ -34,7 +25,7 @@ export const MY_FORMATS = {
             useClass: MomentDateAdapter,
             deps: [MAT_DATE_LOCALE]
         },
-        { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS }
+        { provide: MAT_DATE_FORMATS, useValue: DEFAULT_MATERIAL_DATE_FORMAT }
     ]
 })
 export class ActivityFormComponent implements OnInit {
--- src/utils/patterns/default-date.ts
+++ src/utils/patterns/default-date.ts
@@ -1,1 +1,12 @@
 export const DEFAULT_DATE_FORMAT = 'DD/MM/YYYY';
+export const DEFAULT_MATERIAL_DATE_FORMAT = {
+    parse: {
+        dateInput: 'LL'
+    },
+    display: {
+        dateInput: 'DD/MM/YYYY',
+        monthYearLabel: 'MMM YYYY',
+        dateA11yLabel: 'LL',
+        monthYearA11yLabel: 'MMMM YYYY'
+    }
+};
